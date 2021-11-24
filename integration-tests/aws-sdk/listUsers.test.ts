73c6eafa8ccead1897a149572708a5f225de27c0
James Gregory <james@jagregory.com>
2021-11-24 18:45:20 +1100

    test: list users sdk test

---
+++ integration-tests/aws-sdk/listUsers.test.ts
@@ -0,0 +1,51 @@
+import { withCognitoSdk } from "./setup";
+
+describe(
+  "CognitoIdentityServiceProvider.listUsers",
+  withCognitoSdk((Cognito) => {
+    it("lists users", async () => {
+      const client = Cognito();
+
+      const createUserResult = await client
+        .adminCreateUser({
+          Username: "abc",
+          UserPoolId: "test",
+          TemporaryPassword: "TemporaryPassword", // TODO: shouldn't need to supply this
+        })
+        .promise();
+
+      const result = await client
+        .listUsers({
+          UserPoolId: "test",
+        })
+        .promise();
+
+      expect(result).toEqual({
+        Users: [
+          {
+            Attributes: createUserResult.User?.Attributes,
+            Enabled: true,
+            UserCreateDate: createUserResult.User?.UserCreateDate,
+            UserLastModifiedDate: createUserResult.User?.UserLastModifiedDate,
+            UserStatus: "CONFIRMED",
+            Username: "abc",
+          },
+        ],
+      });
+    });
+
+    it("handles no users", async () => {
+      const client = Cognito();
+
+      const result = await client
+        .listUsers({
+          UserPoolId: "test",
+        })
+        .promise();
+
+      expect(result).toEqual({
+        Users: [],
+      });
+    });
+  })
+);
