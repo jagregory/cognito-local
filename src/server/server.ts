95ec2cf5162413c06ca04e1c9aa3b85550dc6bcd
Michael Ruocco <michael.ruocco@hotmail.com>
2024-05-05 23:30:45 +0100

    fix(server): ensure configured port and hostname are used for issuer and jwks urls

--- src/server/server.ts
+++ src/server/server.ts
@@ -55,11 +55,14 @@ export const createServer = (
     });
   });
 
+  const hostname = process.env.HOST ?? "localhost";
+  const port = process.env.PORT ?? "9229";
+
   app.get("/:userPoolId/.well-known/openid-configuration", (req, res) => {
     res.status(200).json({
       id_token_signing_alg_values_supported: ["RS256"],
-      jwks_uri: `http://localhost:9229/${req.params.userPoolId}/.well-known/jwks.json`,
-      issuer: `http://localhost:9229/${req.params.userPoolId}`,
+      jwks_uri: `http://${hostname}:${port}/${req.params.userPoolId}/.well-known/jwks.json`,
+      issuer: `http://${hostname}:${port}/${req.params.userPoolId}`,
     });
   });
 
